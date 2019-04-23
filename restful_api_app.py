bbdf1aac9ca765581d4d4bb545d3314757ad75f6
Tasnia <tasniakamal@hotmail.co.uk>
2019-04-23 20:02:20 +0100

    Using jsonify function to return task list as JSON data

--- restful_api_app.py
+++ restful_api_app.py
@@ -1,11 +1,31 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+# Array of dictionaries, simpler than using a db for now.
+# u for unicode so special characters do not break it.
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Ibuprofen',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+# Base path to access this service.
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+
+# Flask's jsonify function returns above task data as JSON data.
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 if __name__ == '__main__':
     app.run(debug=True)
